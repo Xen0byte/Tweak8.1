806df5640c8667363107e92c880fee4a4b8848d1
Vlad-Alexandru Tănăsescu <vlad_tanasescu@outlook.com>
2016-03-07 11:16:14 +0000

    UI Done

--- Tweak8.1/Tweak8.1/Form1.Designer.cs
+++ Tweak8.1/Tweak8.1/Form1.Designer.cs
@@ -31,6 +31,21 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label24 = new System.Windows.Forms.Label();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.tabControl2 = new System.Windows.Forms.TabControl();
@@ -63,7 +78,10 @@
             this.tabPage14 = new System.Windows.Forms.TabPage();
             this.tabControl9 = new System.Windows.Forms.TabControl();
             this.tabPage15 = new System.Windows.Forms.TabPage();
+            this.label25 = new System.Windows.Forms.Label();
+            this.label26 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.tabControl2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.tabControl3.SuspendLayout();
@@ -90,12 +108,165 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.label26);
+            this.tabPage1.Controls.Add(this.label25);
+            this.tabPage1.Controls.Add(this.label24);
+            this.tabPage1.Controls.Add(this.label23);
+            this.tabPage1.Controls.Add(this.label22);
+            this.tabPage1.Controls.Add(this.label21);
+            this.tabPage1.Controls.Add(this.label20);
+            this.tabPage1.Controls.Add(this.label19);
+            this.tabPage1.Controls.Add(this.label18);
+            this.tabPage1.Controls.Add(this.label17);
+            this.tabPage1.Controls.Add(this.label16);
+            this.tabPage1.Controls.Add(this.label15);
+            this.tabPage1.Controls.Add(this.label14);
+            this.tabPage1.Controls.Add(this.label13);
+            this.tabPage1.Controls.Add(this.label12);
+            this.tabPage1.Controls.Add(this.label11);
+            this.tabPage1.Controls.Add(this.label10);
             this.tabPage1.Location = new System.Drawing.Point(4, 44);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Size = new System.Drawing.Size(659, 489);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "System Information";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label24
+            // 
+            this.label24.AutoSize = true;
+            this.label24.Location = new System.Drawing.Point(168, 215);
+            this.label24.Name = "label24";
+            this.label24.Size = new System.Drawing.Size(70, 13);
+            this.label24.TabIndex = 14;
+            this.label24.Text = "BIOS Version";
+            // 
+            // label23
+            // 
+            this.label23.AutoSize = true;
+            this.label23.Location = new System.Drawing.Point(168, 240);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(62, 13);
+            this.label23.TabIndex = 13;
+            this.label23.Text = "GPU Model";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Location = new System.Drawing.Point(69, 240);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(52, 13);
+            this.label22.TabIndex = 12;
+            this.label22.Text = "Graphics:";
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(168, 190);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(98, 13);
+            this.label21.TabIndex = 11;
+            this.label21.Text = "BIOS Manufacturer";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(69, 190);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(35, 13);
+            this.label20.TabIndex = 10;
+            this.label20.Text = "BIOS:";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(168, 165);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(55, 13);
+            this.label19.TabIndex = 9;
+            this.label19.Text = "MB Model";
+            this.label19.Click += new System.EventHandler(this.label19_Click);
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(168, 140);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(89, 13);
+            this.label18.TabIndex = 8;
+            this.label18.Text = "MB Manufacturer";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(69, 140);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(70, 13);
+            this.label17.TabIndex = 7;
+            this.label17.Text = "Motherboard:";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(168, 115);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(70, 13);
+            this.label16.TabIndex = 6;
+            this.label16.Text = "RAM Amount";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(69, 115);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(89, 13);
+            this.label15.TabIndex = 5;
+            this.label15.Text = "Physical Memory:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(168, 90);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(92, 13);
+            this.label14.TabIndex = 4;
+            this.label14.Text = " Proc Architecture";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(168, 65);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(64, 13);
+            this.label13.TabIndex = 3;
+            this.label13.Text = " Proc Model";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(69, 65);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(57, 13);
+            this.label12.TabIndex = 2;
+            this.label12.Text = "Processor:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(168, 40);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(46, 13);
+            this.label11.TabIndex = 1;
+            this.label11.Text = " OS Info";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(69, 40);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(93, 13);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Operating System:";
             // 
             // label1
             // 
@@ -219,6 +390,9 @@
             this.label4.Size = new System.Drawing.Size(100, 20);
             this.label4.TabIndex = 5;
             this.label4.Text = "Performance";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            this.label4.MouseEnter += new System.EventHandler(this.label_MouseEnter);
+            this.label4.MouseLeave += new System.EventHandler(this.label_MouseLeave);
             // 
             // label5
             // 
@@ -231,6 +405,9 @@
             this.label5.Size = new System.Drawing.Size(66, 20);
             this.label5.TabIndex = 6;
             this.label5.Text = "Security";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            this.label5.MouseEnter += new System.EventHandler(this.label_MouseEnter);
+            this.label5.MouseLeave += new System.EventHandler(this.label_MouseLeave);
             // 
             // label6
             // 
@@ -243,6 +420,9 @@
             this.label6.Size = new System.Drawing.Size(127, 20);
             this.label6.TabIndex = 7;
             this.label6.Text = "Internet Explorer";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            this.label6.MouseEnter += new System.EventHandler(this.label_MouseEnter);
+            this.label6.MouseLeave += new System.EventHandler(this.label_MouseLeave);
             // 
             // label7
             // 
@@ -255,6 +435,9 @@
             this.label7.Size = new System.Drawing.Size(108, 20);
             this.label7.TabIndex = 8;
             this.label7.Text = "Context Menu";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            this.label7.MouseEnter += new System.EventHandler(this.label_MouseEnter);
+            this.label7.MouseLeave += new System.EventHandler(this.label_MouseLeave);
             // 
             // label8
             // 
@@ -264,9 +447,12 @@
             this.label8.ForeColor = System.Drawing.Color.Teal;
             this.label8.Location = new System.Drawing.Point(12, 336);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(79, 20);
+            this.label8.Size = new System.Drawing.Size(67, 20);
             this.label8.TabIndex = 9;
-            this.label8.Text = "Additional";
+            this.label8.Text = "Network";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            this.label8.MouseEnter += new System.EventHandler(this.label_MouseEnter);
+            this.label8.MouseLeave += new System.EventHandler(this.label_MouseLeave);
             // 
             // label9
             // 
@@ -279,6 +465,9 @@
             this.label9.Size = new System.Drawing.Size(52, 20);
             this.label9.TabIndex = 10;
             this.label9.Text = "About";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            this.label9.MouseEnter += new System.EventHandler(this.label_MouseEnter);
+            this.label9.MouseLeave += new System.EventHandler(this.label_MouseLeave);
             // 
             // pictureBox1
             // 
@@ -463,12 +652,32 @@
             this.tabPage15.Text = "About";
             this.tabPage15.UseVisualStyleBackColor = true;
             // 
+            // label25
+            // 
+            this.label25.AutoSize = true;
+            this.label25.Location = new System.Drawing.Point(69, 265);
+            this.label25.Name = "label25";
+            this.label25.Size = new System.Drawing.Size(37, 13);
+            this.label25.TabIndex = 15;
+            this.label25.Text = "Audio:";
+            // 
+            // label26
+            // 
+            this.label26.AutoSize = true;
+            this.label26.Location = new System.Drawing.Point(168, 265);
+            this.label26.Name = "label26";
+            this.label26.Size = new System.Drawing.Size(84, 13);
+            this.label26.TabIndex = 16;
+            this.label26.Text = "Sound Card Info";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.tabControl2);
             this.Controls.Add(this.tabControl9);
             this.Controls.Add(this.tabControl8);
@@ -486,13 +695,15 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.tabControl1);
-            this.Controls.Add(this.pictureBox1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Tweak8.1";
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.tabControl2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.tabControl3.ResumeLayout(false);
@@ -543,6 +754,23 @@
         private System.Windows.Forms.TabPage tabPage14;
         private System.Windows.Forms.TabControl tabControl9;
         private System.Windows.Forms.TabPage tabPage15;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label23;
+        private System.Windows.Forms.Label label24;
+        private System.Windows.Forms.Label label25;
+        private System.Windows.Forms.Label label26;
     }
 }
 
--- Tweak8.1/Tweak8.1/Form1.cs
+++ Tweak8.1/Tweak8.1/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Management;
+using Microsoft.VisualBasic.Devices;
 
 namespace Tweak8._1
 {
@@ -15,6 +17,150 @@ namespace Tweak8._1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //
+        //      Win32 Provider (for getting all software/hardware info)
+        //
+
+        public static string OSbit()
+        {
+            bool is64bit = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("PROCESSOR_ARCHITEW6432"));
+            if (is64bit == true)
+                return "64-Bit";
+            else
+                return "32-Bit";
+        }
+
+        static ulong GetTotalMemoryInBytes()
+        {
+            return new ComputerInfo().TotalPhysicalMemory;
+        }
+
+        public ulong MemInMB = GetTotalMemoryInBytes() / 1048576;
+
+        static ulong GetAvailableMemoryInBytes()
+        {
+            return new ComputerInfo().AvailablePhysicalMemory;
+        }
+
+        public ulong AvMemInMB = GetAvailableMemoryInBytes() / 1048576;
+
+        static string GetOSVersion()
+        {
+            return new ComputerInfo().OSVersion;
+        }
+
+        public static string SendBackProcessorName()
+        {
+            ManagementObjectSearcher mosProcessor = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
+            string ProcName = null;
+
+            foreach (ManagementObject moProcessor in mosProcessor.Get())
+            {
+                if (moProcessor["name"] != null)
+                {
+                    ProcName = moProcessor["name"].ToString();
+                }
+            }
+
+            ProcName = ProcName
+                   .Replace("(TM)", "™")
+                   .Replace("(tm)", "™")
+                   .Replace("(R)", "®")
+                   .Replace("(r)", "®")
+                   .Replace("(C)", "©")
+                   .Replace("(c)", "©")
+                   .Replace("    ", " ")
+                   .Replace("  ", " ");
+
+            return ProcName;
+        }
+
+        public string cpuName = SendBackProcessorName();
+
+        public static string GetBoardMaker()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_BaseBoard");
+            foreach (ManagementObject wmi in searcher.Get())
+            {
+                try
+                {
+                    return "Manufacturer: " + wmi.GetPropertyValue("Manufacturer").ToString();
+                }
+                catch { }
+            }
+            return "Manufacturer: Unknown";
+        }
+
+        public static string GetBoardModel()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_BaseBoard");
+            foreach (ManagementObject wmi in searcher.Get())
+            {
+                try
+                {
+                    return "Model: " + wmi.GetPropertyValue("Model").ToString();
+                }
+                catch { }
+            }
+            return "Model: Unknown";
+        }
+
+        public static string GetBIOSmanufacturer()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_BIOS");
+            foreach (ManagementObject wmi in searcher.Get())
+            {
+                try
+                {
+                    return "Manufacturer: " + wmi.GetPropertyValue("Manufacturer").ToString();
+                }
+                catch { }
+            }
+            return "Manufacturer: Unknown";
+        }
+
+        public static string GetBIOSvesion()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_BIOS");
+            foreach (ManagementObject wmi in searcher.Get())
+            {
+                try
+                {
+                    return "Version: " + wmi.GetPropertyValue("SMBIOSBIOSVersion").ToString();
+                }
+                catch { }
+            }
+            return "Version: Unknown";
+        }
+
+        public static string GPUname()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_VideoController");
+            foreach (ManagementObject wmi in searcher.Get())
+            {
+                try
+                {
+                    return wmi.GetPropertyValue("Name").ToString();
+                }
+                catch { }
+            }
+            return "GPU: Unknown";
+        }
+
+        public static string SoundCardName()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_SoundDevice");
+            foreach (ManagementObject wmi in searcher.Get())
+            {
+                try
+                {
+                    return wmi.GetPropertyValue("Description").ToString() + " !? (do an 'if primary' check)";
+                }
+                catch { }
+            }
+            return "Sound Card: Unknown";
         }
 
         private void HideAll()
@@ -46,6 +192,16 @@ namespace Tweak8._1
 
             if (i == 1)
             {
+                label11.Text = GetOSFriendlyName() + ", " + OSbit() + " (Build " + GetOSVersion() + ")";
+                label13.Text = cpuName;
+                label14.Text = "Processor Architecture: " + GetCpuArch();
+                label16.Text = MemInMB + " MB of RAM (" + AvMemInMB + " MB Available)";
+                label18.Text = GetBoardMaker();
+                label19.Text = GetBoardModel();
+                label21.Text = GetBIOSmanufacturer();
+                label24.Text = GetBIOSvesion();
+                label23.Text = GPUname();
+                label26.Text = SoundCardName();
                 tabControl1.Visible = true;
                 label1.Font = new Font(label1.Font, FontStyle.Underline | FontStyle.Bold);
             }
@@ -59,7 +215,36 @@ namespace Tweak8._1
                 tabControl3.Visible = true;
                 label3.Font = new Font(label3.Font, FontStyle.Underline | FontStyle.Bold);
             }
-
+            else if (i == 4)
+            {
+                tabControl4.Visible = true;
+                label4.Font = new Font(label4.Font, FontStyle.Underline | FontStyle.Bold);
+            }
+            else if (i == 5)
+            {
+                tabControl5.Visible = true;
+                label5.Font = new Font(label5.Font, FontStyle.Underline | FontStyle.Bold);
+            }
+            else if (i == 6)
+            {
+                tabControl6.Visible = true;
+                label6.Font = new Font(label6.Font, FontStyle.Underline | FontStyle.Bold);
+            }
+            else if (i == 7)
+            {
+                tabControl7.Visible = true;
+                label7.Font = new Font(label7.Font, FontStyle.Underline | FontStyle.Bold);
+            }
+            else if (i == 8)
+            {
+                tabControl8.Visible = true;
+                label8.Font = new Font(label8.Font, FontStyle.Underline | FontStyle.Bold);
+            }
+            else if (i == 9)
+            {
+                tabControl9.Visible = true;
+                label9.Font = new Font(label9.Font, FontStyle.Underline | FontStyle.Bold);
+            }
         }
 
         private void label_MouseEnter(object sender, EventArgs e)
@@ -82,22 +267,94 @@ namespace Tweak8._1
                 label.Font = new Font(label.Font, FontStyle.Regular);
         }
 
-// label1
+        public static string GetOSFriendlyName()
+        {
+            string result = string.Empty;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT Caption FROM Win32_OperatingSystem");
+            foreach (ManagementObject os in searcher.Get())
+            {
+                result = os["Caption"].ToString();
+                break;
+            }
+
+            if (Environment.OSVersion.ServicePack.Length == 0)
+                return result;
+            else
+                return result + " " + Environment.OSVersion.ServicePack;
+        }
+
+        public static string GetCpuArch()
+        {
+            ManagementScope scope = new ManagementScope();
+            ObjectQuery query = new ObjectQuery("SELECT Architecture FROM Win32_Processor");
+            ManagementObjectSearcher search = new ManagementObjectSearcher(scope, query);
+            ManagementObjectCollection results = search.Get();
+
+            ManagementObjectCollection.ManagementObjectEnumerator e = results.GetEnumerator();
+            e.MoveNext();
+            ushort arch = (ushort)e.Current["Architecture"];
+
+            switch (arch)
+            {
+                case 0:
+                    return "x86";
+                case 1:
+                    return "MIPS";
+                case 2:
+                    return "Alpha";
+                case 3:
+                    return "PowerPC";
+                case 6:
+                    return "Itanium";
+                case 9:
+                    return "x64";
+                default:
+                    return "Unknown Architecture (WMI ID " + arch.ToString() + ")";
+            }
+        }
+
+// Labels
         private void label1_Click(object sender, EventArgs e)
         {
             EnableSet(1);
         }
-
-// label2
         private void label2_Click(object sender, EventArgs e)
         {
             EnableSet(2);
         }
-
-// label3
         private void label3_Click(object sender, EventArgs e)
         {
             EnableSet(3);
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+            EnableSet(4);
+        }
+        private void label5_Click(object sender, EventArgs e)
+        {
+            EnableSet(5);
+        }
+        private void label6_Click(object sender, EventArgs e)
+        {
+            EnableSet(6);
+        }
+        private void label7_Click(object sender, EventArgs e)
+        {
+            EnableSet(7);
+        }
+        private void label8_Click(object sender, EventArgs e)
+        {
+            EnableSet(8);
+        }
+        private void label9_Click(object sender, EventArgs e)
+        {
+            EnableSet(9);
+            pictureBox1.Visible = true;
+        }
+
+        private void label19_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
